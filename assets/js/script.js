b32deeaa505372931300bc69fc0cae258068b5b0
Mirjana cale <mirjana083@gmail.com>
2024-05-08 20:26:21 +0000

    Add Event listeners for document louding and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded" ,function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (button of buttons){
+        button.addEventListener("click",function(){
+            if (this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        
+        })
+    }
+    
+})
+
+
+
+
 function runGame(){
 
 }
