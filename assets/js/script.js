3542e7a8258201e2184db3227703753d57db2416
Mirjana cale <mirjana083@gmail.com>
2024-05-12 18:43:43 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,10 @@ if(gameType === "addition"){
     displayAdditionQuestion(num1, num2);
 }else if(gameType === "multiply") {
     displayMultiplyQuestion(num1, num2);
+
     
+} else if (gameType === "subtract") { 
+    displaySubtractQuestion(num1, num2);
 }else{
 (`unknown game type ${gameType}`);
 throw `unknown game type ${gameType} . Aborting!`;
@@ -87,15 +90,17 @@ let operator = document.getElementById("operator").innerText;
 
 if(operator === "+") {
     return [operand1 + operand2 , "addition"];
-}else if(operator === "x") {
+}else if (operator === "x") {
    return [operand1 * operand2, "multiply"]; 
 
-} else {
+} else if ( operator === "-") {
+    return [operand1 - operand2, "subtract"];
+ }else {
+    
     alert(`Unimplemented operator ${operator}`);
     throw `Unimplemented operator ${operator}. Aborting!`;
+ }
 }
-}
-
 
 /**
  * gets the current score from the dom and increments it by 1
@@ -134,7 +139,11 @@ document.getElementById("operator").textContent = "+";
 
 }  
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion( operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
+
 
 }
 
